df6984fa0f8acab5d977c08273f069527426c643
Steve Polito <stevepolito@hey.com>
2024-05-01 05:48:38 -0400

    WIP Parse links

--- app/jobs/audit/webpage/create_job.rb
+++ app/jobs/audit/webpage/create_job.rb
@@ -1,11 +1,20 @@
+# TODO: Why do we need this?
+require "net/http"
+
 class Audit::Webpage::CreateJob < ApplicationJob
   queue_as :default
 
   def perform(webpage)
     response = Net::HTTP.get(webpage.uri)
-    document = Nokogiri::HTML(response)
-    debugger
 
-    webpage.create_document(body: document)
+    webpage.create_document(body: response)
+    links = Nokogiri::HTML(webpage.document.body).css("a")
+
+    links.each do |link|
+      url = link.attributes["href"].value
+      text = link.text
+
+      webpage.document.links.create!(url:, text:)
+    end
   end
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,6 @@
 class Document < ApplicationRecord
   belongs_to :webpage
-  has_one :link
+  has_many :links
 
   # TODO: https://api.rubyonrails.org/classes/ActiveRecord/AttributeMethods/Serialization/ClassMethods.html#method-i-serialize
   # serialize :body, coder: TODO
